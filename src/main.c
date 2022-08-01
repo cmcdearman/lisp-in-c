6032c1ac936ec122f01a164abee4e2b24e825bfc
mathlet <mcdearmanchris@gmail.com>
2022-07-31 20:47:37 -0500

    remove looping over files

--- src/main.c
+++ src/main.c
@@ -6,28 +6,6 @@
 
 int main()
 {
-  TokenStream *tokens = lex(string_from_file("examples/deadSimple.egl"));
-//  while (true)
-//  {
-//    Token *tok = tok_stream_next(tokens);
-//    if (tok->type != TOK_EOF)
-//    {
-//      printf(
-//          "Token { Type: %s, Lit: %s } - <%zu, %zu>\n",
-//          tok_type_to_str(tok->type),
-//          tok->lit,
-//          tok->span->start,
-//          tok->span->end);
-//      tok = NULL;
-//    }
-//    else
-//      break;
-//  }
-  while (tok_stream_peek(tokens)->type != TOK_EOF) {
-    Object *ast = parse(tokens);
-    print_object(ast);
-    printf("\n");
-    tok_stream_next(tokens);
-  }
+  print_object(parse(lex(string_from_file("examples/deadSimple.egl"))));
   return 0;
 }
